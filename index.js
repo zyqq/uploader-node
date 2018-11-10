3e810428a6fad7a8369e9ff69ff4fed16b2c7a31
Frank Fang <FrankFang1990@gmail.com>
2018-11-10 21:04:07 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const upload = multer({ dest: 'uploads/' });
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello nodejs');
+});
+
+app.options('/upload', cors());
+app.post('/upload', cors(), upload.single('file'), (req, res) => {
+  res.send(req.file.filename);
+});
+
+app.get('/preview/:key', cors(), (req, res) => {
+  res.sendFile(
+    `uploads/${req.params.key}`,
+    {
+      root: __dirname,
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+    },
+    error => {
+      console.log(error);
+    }
+  );
+});
+
+app.listen(3000);
